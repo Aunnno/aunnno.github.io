3b4ed433fc6c8b8627c193c41b681c84648bbd6f
Aunnno <2307738723.com>
2025-08-02 23:17:25 +0800

    Site updated: 2025-08-02 23:17:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1754146857210";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1754147841226";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
